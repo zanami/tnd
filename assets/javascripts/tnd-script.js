dc617f1a7403d3156d2218fa2abcee39bc33ad86
zanami <zanami@gmail.com>
2015-09-10 07:39:51 +0200

    tweaks

---
+++ assets/javascripts/tnd-script.js
@@ -0,0 +1,19 @@
+/**
+ * @file
+ * JS for TND.
+ */
+(function ($, Drupal, window, document, undefined) {
+
+  Drupal.behaviors.tnd_accordion = {
+    attach: function(context, setting) {
+      $("#accordion-tnd").on("shown.bs.collapse", function () {
+        var panel = $(this).find('.in').prev('.panel-heading');
+//          var panel = $(this).find('.in');
+
+        $('html, body').animate({
+          scrollTop: panel.offset().top
+        }, 300);
+      });
+    }
+  }
+})(jQuery, Drupal, this, this.document);
